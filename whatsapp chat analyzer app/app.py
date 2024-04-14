1a65ab152d54ffdccff803b45e8edb314f8b86d4
Indrasish <indrasishadhya770@gmail.com>
2024-04-14 21:56:31 +0530

    new

--- whatsapp chat analyzer app/app.py
+++ whatsapp chat analyzer app/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import preprocessor,helper
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 st.title("Whatsapp chat analyzer")
 st.subheader("Analyse your whats app chat")
 uploaded_file = st.file_uploader("Choose a text file")
@@ -19,6 +21,7 @@ if uploaded_file is not None:
     user_list.insert(0,"Overall")
     st.sidebar.title("Analysis")
     selected_user= st.sidebar.selectbox("The analysis with respect to",user_list)
+    # 1st level analysis
     if st.sidebar.button('Show analysis'):
 
         num_messeges,words,media,num_links= helper.fetch_stats(selected_user,df)
@@ -37,7 +40,23 @@ if uploaded_file is not None:
         with col4:
             st.header("Links shared")
             st.subheader(num_links)
+    # Bar chart and percentage of chatting
     if selected_user=="Overall":
+        x,new_df=helper.most_busy_user(df)
+        fig,ax=plt.subplots()
         col1,col2=st.columns(2)
-        
-            
+        with col1:
+            st.header("Most busy user")
+            ax.bar(x.index,x.values, color=['red', 'green', 'blue', 'orange','yellow'])
+            plt.xticks(rotation="vertical")
+            st.pyplot(fig)
+        with col2:
+            st.header("Percentage of chatting")
+            st.dataframe(new_df)
+    # word cloud
+    st.title("Wordcloud")
+    df_wc=helper.create_wordcloud(selected_user,df)
+    fig,ax=plt.subplots()
+    ax.imshow(df_wc)
+    st.pyplot(fig)
+    
--- whatsapp chat analyzer app/helper.py
+++ whatsapp chat analyzer app/helper.py
@@ -1,20 +1,42 @@
 import re
+from wordcloud import WordCloud
+
 def fetch_stats(selected_user,df):
    if selected_user != 'Overall':
       df=df[df['users']==selected_user]
       num_messeges=df.shape[0]
    num_messeges=df.shape[0]
+   
    words=[]
    for message in df['message']:
       words.extend(message.split())
+      
    links=[]
    for message in df['message']:
       url_pattern = r'https?://\S+|www\.\S+' # Regular expression pattern to match URLs
       urls = re.findall(url_pattern,message) # Extract URLs from the text using the regular expression
       links.extend(urls) # Print the extracted URLs
+      
    df=df[df['message']=="<Media omitted>\n"]
    media=df.shape[0]
+   
    return num_messeges,len(words),media,len(links)
+
+# bar chart and percentage of chatting
+def most_busy_user(df):
+   x=df['users'].value_counts().head()
+   df=round(((df['users'].value_counts())/df.shape[0])*100,2).reset_index().rename(columns={'users':'Name','count':'Percent'})
+   return x,df    
+#word cloud
+def create_wordcloud(selected_user,df):
+   if selected_user != 'Overall':
+      df=df[df['users']==selected_user]
+      
+   wc=WordCloud(width=500,height=500,min_font_size=10,background_color="white")
+   df_wc=wc.generate(df['message'].str.cat(sep=" "))
+   return df_wc
+    
+   
 
    
    
